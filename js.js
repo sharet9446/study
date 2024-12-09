d5a43d15a47faeb6964b1f48fd6d21866c0401be
sharet9446 <spees97@naver.com>
2024-12-09 19:38:12 +0900

    코딩테스트

--- js.js
+++ js.js
@@ -1,6 +1,17 @@
-function solution(s) {
-        return parseInt(s, 10)
+function square(n) {
+    for (var x = 1; x <= n; x++){
+        if (n == x * x) {
+            return 1
+        }
+        else {
+           var result = -1
+        }
     }
+    return result
+}
 
-
-console.log(typeof solution("-314"))
+console.log(Number.isInteger(-1.23))
+const x = 'hello world';
+for (let y of x) {
+	console.log(y);
+}
---
+++ 두 정수 사이의 합.js
@@ -0,0 +1,12 @@
+function solution(a, b) {
+    if(a <= b){
+        for(var sum = 0; a <= b; a++){
+            sum = sum + a
+        }
+    }
+    else {
+        for(var sum = 0; a >= b; a--)
+            sum = sum + a
+    }
+    return sum
+}
