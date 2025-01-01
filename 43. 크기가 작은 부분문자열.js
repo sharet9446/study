c115d771d5e497f1c74c73ab67caad1f651783f4
sharet9446 <spees97@naver.com>
2025-01-01 18:48:09 +0900

    개발공부

---
+++ 43. 크기가 작은 부분문자열.js
@@ -0,0 +1,9 @@
+function solution(t, p) {
+    let count = 0;
+    for (let i = 0; i + p.length <= t.length; i++) {
+        if (t.slice(i, i + p.length) <= p) {
+            count++
+        }
+    }
+    return count
+}
