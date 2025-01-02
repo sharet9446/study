b4ce8e7954d08ef56f4061e6d7332ee47df80971
sharet9446 <spees97@naver.com>
2025-01-02 11:10:43 +0900

    개발공부

--- js.js
+++ js.js
@@ -1,36 +1,8 @@
-let a = [1];
-let num = 14;
+let i = 3;
 
-for (let i = 2; i <= num / 2; i++) {
-    if (num % i === 0) {
-        a.push(i)
+while (i< 100) {
+    if (i % 5 === 0) {
+        console.log(i)
     }
+    i++
 }
-
-a.push(num)
-
-
-
-let b = [1, 2, 7, 14]
-
-let c = []
-
-c.push(1, 2, 7, 14)
-
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-console.log(a == b);
-console.log(a == c);
-console.log(b == c);
-
-
-console.log(a[3]);
-console.log(b[3]);
-console.log(c[3]);
-
-console.log(a[3] == b[3]);
-console.log(a[3] == c[3]);
-console.log(b[3] == c[3]);
---
+++ 문자열 내 p와 y의 개수.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    let p = 0;
+    let y = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'p' || s[i] === 'P') { p++ }
+        if (s[i] === 'y' || s[i] === 'Y') { y++ }
+    };
+    return p === y
+};
